16f2a8e9fa2fb90134f746eaaf13338f361d1a67
Gerald Swanson <86332608+gerswanb@users.noreply.github.com>
2025-03-28 16:08:25 -0500

    Update index.php

--- app/views/sexo/index.php
+++ app/views/sexo/index.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listado de Sexos</title>
+    <title>Listados de Sexo</title>
     <link rel="stylesheet" href="/sexo/public/css/style.css">
 </head>
 <body>
 
 <div class="container">
-    <h1>Listado de los  Sexos</h1>
+    <h1>Listados de Sexo</h1>
     <a href="/sexo/app/views/sexo/create.php"><button>Agregar Sexo</button></a>
 
     <table>
